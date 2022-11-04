354f7bc9a3d2b72deb13804565036991a64ec039
Davd Gardner <dave@texasgardners.com>
2022-11-04 17:03:02 -0500

    fixed loader to download my patched version of gitget

--- loader.lua
+++ loader.lua
@@ -4,6 +4,18 @@ local branch = "main"
 local folder = "KhazadDoom-2"
 local gitget = "https://raw.githubusercontent.com/DavidMGardner/cc-tweaked/main/gitget.lua"
 
+local function save(data,file)
+    local file = shell.resolve(file)
+    if not (fs.exists(string.sub(file,1,#file - #fs.getName(file))) and fs.isDir(string.sub(file,1,#file - #fs.getName(file)))) then
+            if fs.exists(string.sub(file,1,#file - #fs.getName(file))) then fs.delete(string.sub(file,1,#file - #fs.getName(file))) end
+            fs.makeDir(string.sub(file,1,#file - #fs.getName(file)))
+    end
+    local f = fs.open(file,"w")
+    f.write(data)
+    f.close()
+end
+
+
 local function download(url, file)
     save(http.get(url).readAll(),file)
 end
